fe2bed11c995c328a187304061e097cae3dcb2b1
Alex Day <alexanderday@email.arizona.edu>
2022-02-06 05:31:48 -0700

    Add learning_logs app views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+def index(request):
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    '''
+    Show all the topics for the user
+    '''
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    '''
+    Show all entries for a topic
+    '''
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
