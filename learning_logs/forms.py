7f5f993a6b6c28b034a21c3674854f64a78cca7f
Alex Day <alexanderday@email.arizona.edu>
2022-02-06 18:47:22 -0700

    Add forms for data manipulation

---
+++ learning_logs/forms.py
@@ -0,0 +1,24 @@
+from socket import fromshare
+from django import forms
+from matplotlib import widgets
+
+from .models import Topic, Entry
+
+class TopicForm(forms.ModelForm):
+    '''
+    Form to create a new topic given user input
+    '''
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text' : ''}
+
+class EntryForm(forms.ModelForm):
+    '''
+    Form to add in a new entry for a given topic
+    '''
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
