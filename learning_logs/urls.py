953badad7ac831e6abef8b91200f18f4b76a6ac9
Alex Day <alexanderday@email.arizona.edu>
2022-02-06 18:47:37 -0700

    Add data manipulation urls

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,6 +7,14 @@ from . import views
 urlpatterns = [
     # home page
     path('', views.index, name='index'),
+    # page displaying all topics
     path('topics', views.topics, name='topics'),
-    path('topics/<int:topic_id>', views.topic, name='topic')
+    # page displaying all entries for a topic
+    path('topics/<int:topic_id>', views.topic, name='topic'),
+    # page for adding new topics
+    path('new_topic', views.new_topic, name='new_topic'),
+    # page for adding new entry for a given topic
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+    # page to edit existing entries
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 ]
